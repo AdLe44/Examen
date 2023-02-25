bd1f59238fbe29a240a6593a3bd3f35ebf40a589
Adri Leal <zc.adrileal@gmail.com>
2023-02-25 15:30:34 -0700

    ruteo correcto de index

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
     session_start();
     if(isset($_SESSION["vjDataSesion"])){
         /* guarda registro de que entro en login */
-        header("views/dashboard.php", true, 0);
+        echo "holi";
+        header("Location: views/dashboard.php", true, 0);
     } else {
         session_destroy();
-        header("views/login.php", true, 0);
+        echo "no holi";
+        header("Location: views/login.php", true, 0);
     }
 ?>
