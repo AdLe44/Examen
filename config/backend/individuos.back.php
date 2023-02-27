eb40e4de484cf77c65aece99d959d393e5b179b2
Adri Leal <zc.adrileal@gmail.com>
2023-02-27 00:11:45 -0700

    consume el nuevo servicio

---
+++ config/backend/individuos.back.php
@@ -0,0 +1,22 @@
+<?php
+    require_once "../db/PDO.php";
+    $id = $_POST['ID']  == 'null' ? null : $_POST['ID'];
+    $nombre = $_POST['NOMBRE']  == 'null' ? null : $_POST['NOMBRE'];
+    $edad = $_POST['EDAD']  == 'null' ? null : $_POST['EDAD'];
+    $direccion = $_POST['DIRECCION']  == 'null' ? null : $_POST['DIRECCION'];
+    $genero = $_POST['GENERO']  == 'null' ? null : $_POST['GENERO'];
+    $salario = $_POST['SALARIO']  == 'null' ? null : $_POST['SALARIO'];
+    $opcion = $_POST['OPCION'];
+    $stmt = $pdo->prepare("CALL SP_ACCIONES_INDIVIDUO(:Id_Acc, :Nombre_Acc, :Edad_Acc, :Direccion_Acc, :Genero_Acc, :Salario_Acc, :Opcion)");
+    $stmt->bindParam(":Id_Acc", $id, PDO::PARAM_INT);
+    $stmt->bindParam(":Nombre_Acc", $nombre, PDO::PARAM_STR);
+    $stmt->bindParam(":Edad_Acc", $descripcion, PDO::PARAM_INT);
+    $stmt->bindParam(":Direccion_Acc", $descripcion, PDO::PARAM_INT);
+    $stmt->bindParam(":Genero_Acc", $descripcion, PDO::PARAM_INT);
+    $stmt->bindParam(":Salario_Acc", $descripcion, PDO::PARAM_STR);
+    $stmt->bindParam(":Opcion", $opcion, PDO::PARAM_STR);
+    $stmt->execute();
+    $resultado = $stmt->fetch(PDO::FETCH_ASSOC);
+    $resultado_json = json_encode($resultado);
+    echo $resultado_json;
+?>
--- views/config/js/inicio.js
+++ views/config/js/inicio.js
@@ -12,7 +12,7 @@ function llamarProcGeneros(){
             console.log(dataTratada);
         }
     };
-    XHTTP.open("POST", "../config/backend/inicio.back.php", true);
+    XHTTP.open("POST", "../config/backend/generos.back.php", true);
     XHTTP.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     let datos = {
         ID: null,
@@ -20,8 +20,36 @@ function llamarProcGeneros(){
         DESCRIPCION: null,
         OPCION: "READ"
     };
-    let parametros = Object.keys(datos).map(function(key) {
+    XHTTP.send(normalizarDatos(datos));
+}
+function llamarProcIndividuos(){
+    XHTTP.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            // Procesar la respuesta del archivo PHP
+            if(!this.response) return console.error("No se obtubo respuesta del servicio");
+            let respuestaNoTratada = JSON.parse(this.response);
+            if(!respuestaNoTratada.Retorno) return console.error("No se obtubo respuesta del servicio");
+            let respuestaTratada = JSON.parse(respuestaNoTratada.Retorno);
+            if(!respuestaTratada.Data) return console.error(respuestaTratada.Message);
+            let dataTratada = JSON.parse(respuestaTratada.Data);
+            console.log(dataTratada);
+        }
+    };
+    XHTTP.open("POST", "../config/backend/individuos.back.php", true);
+    XHTTP.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    let datos = {
+        ID: null,
+        NOMBRE: null,
+        EDAD: null,
+        DIRECCION: null,
+        GENERO: null,
+        SALARIO: null,
+        OPCION: "READ"
+    };
+    XHTTP.send(normalizarDatos(datos));
+}
+function normalizarDatos(datos){
+    return Object.keys(datos).map(function(key) {
         return encodeURIComponent(key) + '=' + encodeURIComponent(datos[key]);
     }).join('&');
-    XHTTP.send(parametros);
 }
